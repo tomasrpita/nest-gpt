d887f3e846612b0e4f65e55aa3c4d4a4b0dddf76
tomasrpita <tomasrpita@gmail.com>
2024-03-23 17:04:13 +0100

    Update dependencies and configuration for @nestjs/config for use .env

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -8,7 +8,9 @@ export const orthographyCheckUseCase = async(options: Options) => {
 
     const { prompt } = options;
 
+
     return {
        prompt: prompt,
+       apikey: process.env.OPENIA_API_KEY,
     }
 }
