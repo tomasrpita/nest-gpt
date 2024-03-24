d2fb1bd1bb63e316ea50808756fd25759578ef01
tomasrpita <tomasrpita@gmail.com>
2024-03-24 10:22:22 +0100

    Add OpenAI dependency and integrate it into the GptService

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
+
+import { OpenAI } from 'openai';
+
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
 
 @Injectable()
 export class GptService {
 
+    private openai = new OpenAI({
+        apiKey: process.env.OPENIA_API_KEY,
+    });
+
     // solo va a llamar casos de uso
     async ortographyCheck(orthographyDto: OrthographyDto) {
         // Also we can use the dto to validate the input
-        return await orthographyCheckUseCase({
+        return await orthographyCheckUseCase(this.openai, {
             prompt: orthographyDto.prompt,
         });
     }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,16 +1,32 @@
+import OpenAI from "openai";
 
 
 interface Options {
     prompt: string;
 }
 
-export const orthographyCheckUseCase = async(options: Options) => {
+export const orthographyCheckUseCase = async(openai: OpenAI,options: Options) => {
 
     const { prompt } = options;
 
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { 
+                role: "system", 
+                content: "Eres un asistente amable, tu nombre es Chapty." 
+            }, 
+            { 
+                role: "user", 
+                content: prompt
+            }
+        ],
+        model: "gpt-3.5-turbo",
+        
+    });
 
-    return {
-       prompt: prompt,
-       apikey: process.env.OPENIA_API_KEY,
-    }
+    console.log(completion);
+
+    return completion.choices[0];
+
+
 }
