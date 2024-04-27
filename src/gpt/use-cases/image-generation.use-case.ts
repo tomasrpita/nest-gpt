47a7f3db419158b92514fcea29f676cd03c3e1db
tomasrpita <tomasrpita@gmail.com>
2024-04-27 12:28:37 +0200

    feat: Implement image generation use case with DALL-E model

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -11,5 +11,24 @@ export const imageGenerationUseCase = async (
   options: Options,
 ) => {
   const { prompt, originalImage, maskImage } = options;
-  console.log({ prompt, originalImage, maskImage });
+
+  // TODO: verificar originalImage e maskImage
+  const response = await openai.images.generate({
+    prompt: prompt,
+    model: 'dall-e-3',
+    n: 1,
+    size: '1024x1024',
+    quality: 'standard',
+    response_format: 'url',
+  });
+
+  // TODO: guardar la imagen en FS.
+
+  console.log(response);
+
+  return {
+    url: response.data[0].url,
+    localPath: '',
+    revised_prompt: response.data[0].revised_prompt,
+  };
 };
