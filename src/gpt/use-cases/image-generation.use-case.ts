85dc0a4cfb86100a185b3d470691940a5b121477
tomasrpita <tomasrpita@gmail.com>
2024-04-28 09:50:43 +0200

    feat: Update image generation use case to support base64 mask image

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,8 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 import OpenAI from 'openai';
-import { downloadImageAsPng } from 'src/helpers';
+import { downloadBase64ImageAsPng, downloadImageAsPng } from 'src/helpers';
 
 interface Options {
   prompt: string;
@@ -13,23 +16,51 @@ export const imageGenerationUseCase = async (
 ) => {
   const { prompt, originalImage, maskImage } = options;
 
-  // TODO: verificar originalImage e maskImage
-  const response = await openai.images.generate({
+  // verificar originalImage e maskImage
+  if (!originalImage || !maskImage) {
+    const response = await openai.images.generate({
+      prompt: prompt,
+      model: 'dall-e-3',
+      n: 1,
+      size: '1024x1024',
+      quality: 'standard',
+      response_format: 'url',
+    });
+
+    // Guardar la imagen en FS.
+    const url = downloadImageAsPng(response.data[0].url);
+
+    // console.log(response);
+
+    return {
+      url: url, //TODO: http://localhost:3000/gpt/image-generation/1703770602518,png
+      OpenAIUrl: response.data[0].url,
+      revised_prompt: response.data[0].revised_prompt,
+    };
+  }
+
+  // originalImage = http://localhost:3000/gpt/image-generation/1703770602518,png
+  // maskImage=Base64;klsañlkrwelifnmasdklñjfasdkjflñkasdjflkñasdjflkasdjfasdjf
+  const pngImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadBase64ImageAsPng(maskImage);
+
+  const response = await openai.images.edit({
+    model: 'dall-e-2',
     prompt: prompt,
-    model: 'dall-e-3',
+    image: fs.createReadStream(pngImagePath),
+    mask: fs.createReadStream(maskPath),
     n: 1,
     size: '1024x1024',
-    quality: 'standard',
     response_format: 'url',
   });
 
-  // Guardar la imagen en FS.
-  const url = downloadImageAsPng(response.data[0].url);
+  const localImagePath = await downloadImageAsPng(response.data[0].url);
+  const fileName = path.basename(localImagePath);
 
-  console.log(response);
+  const publicUrl = `localhost:3000/${fileName}`;
 
   return {
-    url: url,
+    url: publicUrl, //TODO: http://localhost:3000/gpt/image-generation/1703770602518,png
     OpenAIUrl: response.data[0].url,
     revised_prompt: response.data[0].revised_prompt,
   };
